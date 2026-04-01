35a969e48deab3944178be8fd2c4ed292e1fda55
Raimpz <raimond.kurm@gmail.com>
2026-04-01 19:33:43 +0300

    feat: #17 - add edit and delete functionality

---
+++ src/main/java/com/wordcloud/core/dto/UpdateWordRequest.java
@@ -0,0 +1,16 @@
+package com.wordcloud.core.dto;
+
+public class UpdateWordRequest {
+
+    private String word;
+
+    public UpdateWordRequest() {}
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+}
